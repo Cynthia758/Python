39110504bf9a8d43d1a9bbf45b1f809558a32732
Cynthia <parmarvaidehi437@gmail.com>
2025-07-03 09:49:53 +0530

    first commit

---
+++ data-types.py
@@ -0,0 +1,6 @@
+# Basic types - integer, float, complex, boolean, string
+# Container types - list, tuples, sets, dictionary
+# User-defined types - class 
+
+print(4) # no matter how long is the int the range is from 1 to 1e309; on 1e309 it will show output as infinite;
+print(1e309)

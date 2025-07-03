6534ba43d0e8321ef13890d608dc7da3aeb143b9
Cynthia <parmarvaidehi437@gmail.com>
2025-07-03 14:21:47 +0530

    Variables

---
+++ comments.py
@@ -0,0 +1,4 @@
+# Comments
+
+# To enhance the readability of code...
+# We cannot write muitiple lines comment.
--- data-types.py
+++ data-types.py
@@ -2,5 +2,32 @@
 # Container types - list, tuples, sets, dictionary
 # User-defined types - class 
 
-print(4) # no matter how long is the int the range is from 1 to 1e309; on 1e309 it will show output as infinite;
+# int
+print(4) # no matter how long is the int ;the range is from 1 to 1e309; on 1e309 it will show output as infinite;
 print(1e309)
+# boolean
+print(True)
+print(False)
+
+#complex
+print(4+5j)
+
+# string
+print('Nadiad')
+print("Nadiad")
+print("""Nadiad""")
+
+# ********Container Types**********
+
+# list
+
+print([1,2,3,4,5])
+
+# tuple
+print((1,2,3,4,5))
+
+# sets
+print({1,2,3,4,5})
+
+# dict
+print({"Name":"Vaidehi","Age":"17","Gender":"Female"})

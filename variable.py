d25b9349de6bc993c7409319f1710386266ee312
Cynthia <parmarvaidehi437@gmail.com>
2025-07-03 16:06:31 +0530

    learnt variables

--- variable.py
+++ variable.py
@@ -1,2 +1,12 @@
-# int a = 5
-name = "Vaidehi"
+# int a = 5 for other langy=uages
+name = "Vaidehi" # Dynamic typing
+name = True
+name = 4
+print(name)
+
+a = 5; b= 6; c = 7;
+a,b,c = 4,5,6;
+print(a) 
+print(b)
+print(c)
+

42943cd030fb8ffd9412eef1ba0101091a96c1d0
Cynthia <parmarvaidehi437@gmail.com>
2025-07-04 18:16:42 +0530

    Operators

---
+++ operators.py
@@ -0,0 +1,53 @@
+# arithmatic operations
+x = 3;
+y = 7;
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x%y)
+print(x**y)
+print(x>=y)  # comparision or relational
+
+# Logical operators
+x = True
+y = False
+
+print(x or y)
+print(x and y)
+print(not x)
+print(not y)
+
+# Bitwise
+
+x = 2 # Binary value of 2 : 010
+y = 3 # Binary value of 3 : 110
+print(x & y)
+print(x|y)
+
+# Assignment
+a = 3
+print(a)
+a += 3
+# a = a+3
+print(a)
+
+# Identity operator
+a = 3
+b = 3
+print(a is b)
+x = "hello"
+y = "Hello"
+print(x is y)
+
+p = [1,2,3]
+q = [1,2,3]
+print(p is q) # you'll recieve false so that means that if things looks similar it doesn't mean that they are stored at same place.
+
+v = "Hello-World"
+w = "Hello-World"
+print(v is w)
+ 
+
+
+

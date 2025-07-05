6162c800245c5a3ac6f42146d8eb3f6900415034
Cynthia <parmarvaidehi437@gmail.com>
2025-07-05 18:55:20 +0530

    Loops

---
+++ while-loop.py
@@ -0,0 +1,14 @@
+# Loops
+# 1. While
+# 2. Do while
+# 3. for
+# while loop
+# while(condition){
+# code
+# }
+
+number = int(input("Enter the number : "))
+i = 1 
+while i<11:
+    print(number,"*",i,"=",number *i)
+    i +=1

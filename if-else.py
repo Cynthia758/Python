c702dd560fb64b005e08593b9b7cea2a13cce95a
Cynthia <parmarvaidehi437@gmail.com>
2025-07-05 09:34:16 +0530

    Identation

--- if-else.py
+++ if-else.py
@@ -1,6 +1,16 @@
-email = input("Email: ")
-password = input("Your Password: ")
-if email == "parmarvaidehi437@gmail.com" and password == "vai437dehiparmar" :
-    print("WELCOME.")
-else:
-    print("Incorrect credentials.")
+email = input("Email : ")
+if '@' in email :
+    password = input("Enter your password : ")
+    if email == "parmarvaidehi437@gmail.com" and password == "vai437dehiparmar" :
+        print("WELCOME.")
+    elif email == "parmarvaidehi437@gmail.com" and password != "vai437dehiparmar" :
+        print("Password Incorrect")
+        password = input("Enter your password again : ")
+        if password == "vai437dehiparmar" :
+            print("Correct.")
+        else : 
+            print("Still Incorrect.Try Again!!")
+    else:
+        print("Incorrect Credentials.")
+else : 
+    print("Check your Email.")

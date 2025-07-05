ee698a5b700f22eb32190ec0a0032dd79d7772bc
Cynthia <parmarvaidehi437@gmail.com>
2025-07-05 12:37:58 +0530

    indentation

---
+++ indentation.py
@@ -0,0 +1,12 @@
+# indentation : Should apply space : if ke niche else print ke niche print kind of that way ....
+# That thing is known as an indentation.
+# Indentation code readability ko badhata hai !
+
+name = "xyz"
+if name == "xyz" :
+    print('line1')
+    print('line2')
+    if 5 == 5:
+        print('line5')
+else:
+    print('line3')

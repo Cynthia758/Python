acc0ad4f836515605ae0176a488a84c1743ca04e
Cynthia <parmarvaidehi437@gmail.com>
2025-07-10 08:51:25 +0530

    Modification

--- for-loop.py
+++ for-loop.py
@@ -2,7 +2,7 @@
 range(1, 11) # 1 will be included, but 11 will not.
 list(range(1, 11))
 
-# list(range(1,11,2)) ; here 2 signifinace the difference between the numbers so the outcome will be ...
+# list(range(1,11,2)) ; here 2 signifies the difference between the numbers so the outcome will be ...
 # [1, 3, 5, 7, 9]
 
 for i in range(1, 11) :
@@ -15,5 +15,7 @@ for i in range(10,1,-1) :
     print(i)
 print('It can also be used like this..')
 for i in "Kolkata" :
+    print(i)
+for i in 'Vaidehi' :
     print(i)
   

3513596611013ed98a398d6d345beba25a87c2f2
Cynthia <parmarvaidehi437@gmail.com>
2025-07-10 14:55:25 +0530

    Turtle-module

--- turtle_drawing.py
+++ turtle_drawing.py
@@ -3,25 +3,27 @@ import random # Let us pick up a random color
 
 # Create screen and Turtle
 screen = turtle.Screen() # Creates Drawing Window.
+screen.bgcolor("black")
 t = turtle.Turtle() # Drawing pen
 t.speed(10) # 1 = slowest, 10 = fast, 0 = instant
 
 colors = ["red", "orange", "yellow", "pink", "green", "purple"]
 
+
 for i in range(36): # Loop runs for 36 times
     t.color(random.choice(colors)) # Randomly picks color
     t.forward(100) # Moves the pen forward 100 times
-    t.right(60) #turns the turtle 60 degree right
+    t.right(100) #turns the turtle 60 degree right
     t.forward(100)
-    t.right(60)
+    t.right(100)
     t.forward(100) 
-    t.right(60) 
+    t.right(100) 
     t.forward(100) 
-    t.right(60)
+    t.right(100)
     t.forward(100) 
-    t.right(60)
+    t.right(100)
     t.forward(100) 
-    t.right(60)
+    t.right(100)
 
     t.right(10) # Rotate for next pattern
 

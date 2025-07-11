2329360fb0e45e04f07625a7f936c78dbca3aa8d
Cynthia <parmarvaidehi437@gmail.com>
2025-07-11 19:35:44 +0530

    break-continue-pass

--- break-continue-pass.py
+++ break-continue-pass.py
@@ -2,3 +2,18 @@ for i in range(1,11):
     if i == 5:
         break
     print(i)
+    # tO TERMINATE LOOP at a certain point..
+    # ********* Continue Statement***********
+
+for i in range(1,11):
+        if i == 5:
+            continue # 5 will not print...
+        print(i)           
+
+# ****************Pass Statement***********
+
+# Used when we are not knowing what write at a certain time so if we write pass then it will give us no error;
+for i in range(1,11):
+     pass
+# It won't give any error.
+

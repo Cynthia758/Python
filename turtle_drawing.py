675f551190804edae33c5457f1c659ac94feb9bd
Cynthia <parmarvaidehi437@gmail.com>
2025-07-11 16:43:59 +0530

    mandala-Art

--- turtle_drawing.py
+++ turtle_drawing.py
@@ -13,17 +13,17 @@ colors = ["red", "orange", "yellow", "pink", "green", "purple"]
 for i in range(36): # Loop runs for 36 times
     t.color(random.choice(colors)) # Randomly picks color
     t.forward(100) # Moves the pen forward 100 times
-    t.right(100) #turns the turtle 60 degree right
+    t.right(20) #turns the turtle 60 degree right
     t.forward(100)
-    t.right(100)
+    t.right(20)
     t.forward(100) 
-    t.right(100) 
+    t.right(20) 
     t.forward(100) 
-    t.right(100)
+    t.right(20)
     t.forward(100) 
-    t.right(100)
+    t.right(20)
     t.forward(100) 
-    t.right(100)
+    t.right(20)
 
     t.right(10) # Rotate for next pattern
 

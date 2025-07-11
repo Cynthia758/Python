0a8bb7280d79e626d9fc6d0b914e7bfc2eb0e3df
Cynthia <parmarvaidehi437@gmail.com>
2025-07-11 22:36:19 +0530

    builtin-module

---
+++ builtin-modules.py
@@ -0,0 +1,22 @@
+# Module : A file containing a set of functions,Variables,Classes u want to include in your application.
+# kisi aur language me jisko library bolte ho use hi python me module kehte hai ...
+# ***Examples***
+# 1. Math
+import math
+print(math.pi)
+print(math.e)
+# There are lots which we can see after clicking on the tab keyyyy.
+print(math.factorial(4))
+print(math.ceil(8.1))
+print(math.floor(9.9))
+# 2. Random
+import random
+print(random.randint(1,10000))
+a = [1,2,3,9686,46712, 'Vaidehi']
+random.shuffle(a)
+print(a)
+# 3. Time
+import time
+print(time.time)
+print(time.ctime)
+

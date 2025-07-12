0c6558859708946e5ba6e03cd270d28abbe4624e
Cynthia <parmarvaidehi437@gmail.com>
2025-07-12 09:41:43 +0530

    Strings

---
+++ string-1.py
@@ -0,0 +1,9 @@
+# Strings are sequence of characters.
+# In python specifically,Strings are a sequence of Unicode Characters.
+# That means we can add emojis,Other language letters as well
+c = str("Hello")
+print(c)
+a = '''Vaidehi'''
+print(a)
+b="""Vaidehi"""
+print(b)

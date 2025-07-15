26102926222f8a6d92dfae14dac9f9358f441de4
Cynthia <parmarvaidehi437@gmail.com>
2025-07-15 21:40:37 +0530

    Editing

---
+++ editing-deleting-str.py
@@ -0,0 +1,5 @@
+# Editing and Deleting in Strings.
+c = 'Hello'
+print(c)
+c[0] = 'X'
+# Strings are Immutable datatypes in Python that means that after defining c I cannot change it's letter which is on index value 0 
